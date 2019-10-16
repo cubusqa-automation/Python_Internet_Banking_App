64180cdefdce9ed2c1a24026dab69cb1bf009df2
vijay <vijayjeyaram@gmail.com>
2019-10-16 14:55:40 +0530

    Code changed

--- Internet_Banking_4.0/authentication/Retrieve_UserName_test.py
+++ Internet_Banking_4.0/authentication/Retrieve_UserName_test.py
@@ -28,7 +28,6 @@ class Test_Retrieve_UserName:
         deployment_Environment = "osx"
         browser = "chrome"
         remote_Machine = 'http://192.168.200.126:4444/wd/hub'
-        #remote_Machine = 'http://192.168.200.197:4444/wd/hub'
 
         logging.info(
             "Started:Internet Banking Reset Password Flow. FileName: Rest_Password_test.py, ClassName:Test_Reset_Password, TestName:test_Rest_Password")
