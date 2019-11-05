7d0885b09afe13f9a518ee65f1020d8fbc28ae30
vijay <vijayjeyaram@gmail.com>
2019-11-05 17:55:51 +0530

    Created Demo Project

--- Mobile_Internet_Banking/authentication/LoginFlow_test.py
+++ Mobile_Internet_Banking/authentication/LoginFlow_test.py
@@ -41,17 +41,17 @@ class Test_LoginFlow:
                     self.driver = webdriver.Remote(command_executor=general_data.remote_Machine,
                                                    desired_capabilities={'browserName': 'chrome',
                                                                          'javascriptEnabled': True})
-                    # logging.debug("Cloud-OSX-Chrome Browser has been launched.")
+                    logging.debug("Cloud-OSX-Chrome Browser has been launched.")
 
                 else:  # firefox
                     options = webdriver.FirefoxOptions()
                     options.add_argument("--ignore-certificate-errors")
                     options.add_argument("--ignore-ssl-errors")
-                    # options.add_experimental_option("detach", True)
+                    options.add_experimental_option("detach", True)
                     self.driver = webdriver.Remote(command_executor=general_data.remote_Machine,
                                                    desired_capabilities={'browserName': 'firefox',
                                                                          'javascriptEnabled': True})
-                    # logging.debug("Cloud-OSX-Firefox Browser has been launched.")
+                    logging.debug("Cloud-OSX-Firefox Browser has been launched.")
 
             elif general_data.deployment_Environment == "linux":
                 if general_data.mobile_Browser == "chrome":
@@ -62,17 +62,17 @@ class Test_LoginFlow:
                     self.driver = webdriver.Remote(command_executor=general_data.remote_Machine,
                                                    desired_capabilities={'browserName': 'chrome',
                                                                          'javascriptEnabled': True})
-                    # logging.debug("Cloud-Linux-Chrome Browser has been launched.")
+                    logging.debug("Cloud-Linux-Chrome Browser has been launched.")
 
                 else:  # firefox
                     options = webdriver.FirefoxOptions()
                     options.add_argument("--ignore-certificate-errors")
                     options.add_argument("--ignore-ssl-errors")
-                    # options.add_experimental_option("detach", True)
+                    #options.add_experimental_option("detach", True)
                     self.driver = webdriver.Remote(command_executor=general_data.remote_Machine,
                                                    desired_capabilities={'browserName': 'firefox',
                                                                          'javascriptEnabled': True})
-                    # logging.debug("Cloud-Linux-Firefox Browser has been launched.")
+                    logging.debug("Cloud-Linux-Firefox Browser has been launched.")
 
             else:  # windows
                 if general_data.mobile_Browser == "chrome":
@@ -83,17 +83,17 @@ class Test_LoginFlow:
                     self.driver = webdriver.Remote(command_executor=general_data.remote_Machine,
                                                    desired_capabilities={'browserName': 'chrome',
                                                                          'javascriptEnabled': True})
-                    # logging.debug("Cloud-Windows-Chrome Browser has been launched.")
+                    logging.debug("Cloud-Windows-Chrome Browser has been launched.")
 
                 else:  # firefox
                     options = webdriver.FirefoxOptions()
                     options.add_argument("--ignore-certificate-errors")
                     options.add_argument("--ignore-ssl-errors")
-                    # options.add_experimental_option("detach", True)
+                    options.add_experimental_option("detach", True)
                     self.driver = webdriver.Remote(command_executor=general_data.remote_Machine,
                                                    desired_capabilities={'browserName': 'firefox',
                                                                          'javascriptEnabled': True})
-                    # logging.debug("Cloud-Windows-Firefox Browser has been launched.")
+                    logging.debug("Cloud-Windows-Firefox Browser has been launched.")
 
         else:  # on premise
             if general_data.deployment_Environment == "osx":
@@ -104,16 +104,16 @@ class Test_LoginFlow:
                     options.add_experimental_option("detach", True)
                     self.driver = webdriver.Chrome(options=options,
                                                    executable_path="../Browser_Drivers/OSX/chromedriver")
-                    # logging.debug("On Premise-OSX-Chrome Browser has been launched.")
+                    logging.debug("On Premise-OSX-Chrome Browser has been launched.")
 
                 else:  # firefox
                     options = webdriver.FirefoxOptions()
                     options.add_argument("--ignore-certificate-errors")
                     options.add_argument("--ignore-ssl-errors")
-                    # options.add_experimental_option("detach", True)
+                    options.add_experimental_option("detach", True)
                     self.driver = webdriver.Chrome(options=options,
                                                    executable_path="../Browser_Drivers/OSX/geckodriver")
-                    # logging.debug("On Premise-OSX-Firefox Browser has been launched.")
+                    logging.debug("On Premise-OSX-Firefox Browser has been launched.")
 
             elif general_data.deployment_Environment == "linux":
                 if general_data.mobile_Browser == "chrome":
@@ -123,16 +123,16 @@ class Test_LoginFlow:
                     options.add_experimental_option("detach", True)
                     self.driver = webdriver.Chrome(options=options,
                                                    executable_path="../Browser_Drivers/Linux/chromedriver")
-                    # logging.debug("On Premise-Linux-Chrome Browser has been launched.")
+                    logging.debug("On Premise-Linux-Chrome Browser has been launched.")
 
                 else:  # firefox
                     options = webdriver.FirefoxOptions()
                     options.add_argument("--ignore-certificate-errors")
                     options.add_argument("--ignore-ssl-errors")
-                    # options.add_experimental_option("detach", True)
+                    #options.add_experimental_option("detach", True)
                     self.driver = webdriver.Chrome(options=options,
                                                    executable_path="../Browser_Drivers/Linux/geckodriver")
-                    # logging.debug("On Premise-Linux-Firefox Browser has been launched.")
+                    logging.debug("On Premise-Linux-Firefox Browser has been launched.")
 
             else:  # windows
                 if general_data.mobile_Browser == "chrome":
@@ -142,26 +142,26 @@ class Test_LoginFlow:
                     options.add_experimental_option("detach", True)
                     self.driver = webdriver.Chrome(options=options,
                                                    executable_path="../Browser_Drivers/Windows/chromedriver.exe")
-                    # logging.debug("On Premise-Windows-Chrome Browser has been launched.")
+                    logging.debug("On Premise-Windows-Chrome Browser has been launched.")
 
                 else:  # firefox
                     options = webdriver.FirefoxOptions()
                     options.add_argument("--ignore-certificate-errors")
                     options.add_argument("--ignore-ssl-errors")
-                    # options.add_experimental_option("detach", True)
+                    #options.add_experimental_option("detach", True)
                     self.driver = webdriver.Chrome(options=options,
                                                    executable_path="../Browser_Drivers/Windows/geckodriver.exe")
-                    # logging.debug("On Premise-Windows-Firefox Browser has been launched.")
+                    logging.debug("On Premise-Windows-Firefox Browser has been launched.")
 
         self.driver.maximize_window()
         self.driver.get(general_data.mobile_URL)
-        # logging.debug("Internet Banking URL has been passed to the browser.")
+        logging.debug("Internet Banking URL has been passed to the browser.")
         time.sleep(5)
 
     @staticmethod
     def close_browser(self):
         self.driver.close()
-        # logging.info("End:Internet Banking Login Work Flow. FileName: LoginFlow_test.py, ClassName:Test_LoginFlow, TestName:test_LoginFlow")
+        logging.info("End:Internet Banking Login Work Flow. FileName: LoginFlow_test.py, ClassName:Test_LoginFlow, TestName:test_LoginFlow")
 
     def test_LoginFlow(self, startup):
 
@@ -180,5 +180,4 @@ class Test_LoginFlow:
             mobile_dashboard.click_logout_btn()
 
         except Exception as e:
-            print(e)
-            # logging.error("ERROR: Issue in --test_LoginFlow() Method.--", e)
+            logging.error("ERROR: Issue in --test_LoginFlow() Method.--", e)
