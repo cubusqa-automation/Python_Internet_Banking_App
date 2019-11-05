67bbb7f58eed23b2a41a1742b198aa6da646f263
vijay <vijayjeyaram@gmail.com>
2019-11-05 13:13:40 +0530

    Created Demo Project

---
+++ Mobile_Internet_Banking/helper/TestData_Properties.py
@@ -0,0 +1,44 @@
+class General_Properties:
+    mongodb_connection_string = "mongodb+srv://cubusqa-automation:!Cu2010bus@cluster0-1hi04.mongodb.net/test?retryWrites=true&w=majority"
+
+    deployment_Type = None
+    deployment_Environment = None
+    remote_Machine = None
+    admin_Browser = None
+    admin_CUDeposite_Browser = None
+    olb_Browser = None
+    mobile_Browser = None
+    quickPay_PreLogin_Browser = None
+    admin_URL = None
+    olb_URL = None
+    mobile_URL = None
+    quickPay_PreLogin_URL = None
+
+
+class Authentication_Properties:
+    loginFlow_MemberNo = None
+    loginFlow_UserName = None
+    loginFlow_Password = None
+    loginFlow_SSN = None
+    loginFlow_DOB = None
+    retrieveUserName_MemberNo = None
+    retrieveUserName_Password = None
+    retrieveUserName_SSN = None
+    retrieveUserName_DOB = None
+    resetPassword_UserName = None
+    resetPassword_SSN = None
+    resetPassword_DOB = None
+    resetPassword_New_Confirm_Password = None
+
+
+
+
+
+
+
+
+
+
+
+
+
