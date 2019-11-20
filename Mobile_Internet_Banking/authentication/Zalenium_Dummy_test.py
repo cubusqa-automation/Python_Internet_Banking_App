bd49799cbe2e7ee33956491b449cc151e321a4cb
vijay <vijayjeyaram@gmail.com>
2019-11-20 20:19:28 +0530

    Created Demo Project

--- Mobile_Internet_Banking/authentication/Zalenium_Dummy_test.py
+++ Mobile_Internet_Banking/authentication/Zalenium_Dummy_test.py
@@ -3,6 +3,8 @@ import time
 import os
 import subprocess
 
+import paramiko
+
 sys.path.insert(0, os.path.join(os.getcwd(), '..', '..'))
 
 from Mobile_Internet_Banking.helper.TestData import *
@@ -11,16 +13,17 @@ from Mobile_Internet_Banking.helper.TestData import *
 class Test_Zalenium_Dummy:
     def test_zalenium_dummy(self):
 
-        time.sleep(30)
+        #time.sleep(30)
         test_data = TestData()
         general_data = test_data.get_general_test_data()
         print(general_data.report_Machine)
 
-        #bashCommand = "sudo docker cp authentication_container:/Mobile_Internet_Banking/Allure_Report ~/Documents"
-        #subprocess.call(bashCommand)
-
-        command = os.popen('sudo docker cp authentication_container:/Mobile_Internet_Banking/Allure_Report ~/Documents')
-        command.read()
-        command.close()
-
-        time.sleep(5)
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(general_data.report_Machine, username="cubusqa-client", password="@Cu2010bus")
+        sftp = ssh.open_sftp()
+        localpath = '/Mobile_Internet_Banking/Allure_Report'
+        remotepath = '~/Downloads'
+        sftp.put(localpath, remotepath)
+        sftp.close()
+        ssh.close()
