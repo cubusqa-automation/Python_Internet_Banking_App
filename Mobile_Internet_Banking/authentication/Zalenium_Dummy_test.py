8361c66e27b11e916e4a935ba5b022c34ba398c1
vijay <vijayjeyaram@gmail.com>
2019-11-20 20:54:19 +0530

    Created Demo Project

--- Mobile_Internet_Banking/authentication/Zalenium_Dummy_test.py
+++ Mobile_Internet_Banking/authentication/Zalenium_Dummy_test.py
@@ -13,17 +13,8 @@ from Mobile_Internet_Banking.helper.TestData import *
 class Test_Zalenium_Dummy:
     def test_zalenium_dummy(self):
 
-        #time.sleep(30)
+        time.sleep(30)
         test_data = TestData()
         general_data = test_data.get_general_test_data()
         print(general_data.report_Machine)
 
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(general_data.report_Machine, username="cubusqa-client", password="@Cu2010bus")
-        sftp = ssh.open_sftp()
-        localpath = '/Mobile_Internet_Banking/Allure_Report'
-        remotepath = '~/Downloads'
-        sftp.put(localpath, remotepath)
-        sftp.close()
-        ssh.close()
