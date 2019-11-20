2cf525bc60958c3ee94e49c19e2118be149cd041
vijay <vijayjeyaram@gmail.com>
2019-11-20 17:26:47 +0530

    Created Demo Project

--- Mobile_Internet_Banking/authentication/Zalenium_Dummy_test.py
+++ Mobile_Internet_Banking/authentication/Zalenium_Dummy_test.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import os
+import subprocess
 
 sys.path.insert(0, os.path.join(os.getcwd(), '..', '..'))
 
@@ -15,7 +16,7 @@ class Test_Zalenium_Dummy:
         general_data = test_data.get_general_test_data()
         print(general_data.report_Machine)
 
-        #bashCommand = "sudo docker cp authentication_container:/Mobile_Internet_Banking/Allure_Report ~/Documents"
-        #os.subprocess.call(bashCommand)
+        bashCommand = "sudo docker cp authentication_container:/Mobile_Internet_Banking/Allure_Report ~/Documents"
+        subprocess.call(bashCommand)
 
         time.sleep(5)
